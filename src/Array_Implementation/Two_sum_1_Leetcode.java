8ef6ef551805b364e2dcc0f7a25a811d8c47c169
rishabhdev040 <rishabhsingh.dev0@gmail.com>
2025-11-27 16:15:01 +0530

    two sum added problem number 1 leetcode pushed

--- src/Array_Implementation/Two_sum_1_Leetcode.java
+++ src/Array_Implementation/Two_sum_1_Leetcode.java
@@ -10,11 +10,30 @@ package Array_Implementation;
 //You can return the answer in any order.
 
 
+import java.util.ArrayList;
 
 public class Two_sum_1_Leetcode {
     public static void main(String[] args) {
+        // The initial target value we are using to implement two sum.
+        int target = 7;
 
+        //using ArrayList Collection Framework for storing the list of the indices which will be produced in the answer.
+        ArrayList<Integer> ans_list = new ArrayList<Integer>();
+
+        //initializing the sample array
         int [] nums = {0,1,2,3,4,5,6,7,8,9};
+        //looping and conditionals
+        for (int i=0,j=1;j<nums.length;i++,j++){
+            if (nums[i]+nums[j] == target){
+
+                ans_list.add(i);
+                ans_list.add(j);
+
+                break;
+            }
+        }
+
+        System.out.println(ans_list);
 
 
 
