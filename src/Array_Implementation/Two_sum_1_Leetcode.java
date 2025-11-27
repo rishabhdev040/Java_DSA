fbf90a312c7f8636353a968f4fe38ac9e9cb379e
rishabhdev040 <rishabhsingh.dev0@gmail.com>
2025-11-27 14:57:08 +0530

    issues removed

--- src/Array_Implementation/Two_sum_1_Leetcode.java
+++ src/Array_Implementation/Two_sum_1_Leetcode.java
@@ -15,9 +15,6 @@ public class Two_sum_1_Leetcode {
     public static void main(String[] args) {
 
         int [] nums = {0,1,2,3,4,5,6,7,8,9};
-        for(int x :nums){
-            System.out.println(x);
-        }
 
 
 
--- src/LinkedList_Implementation/Remove_nthNode_EndOfLL_19_LeetCode.java
+++ src/LinkedList_Implementation/Remove_nthNode_EndOfLL_19_LeetCode.java
@@ -22,5 +22,37 @@ public class Remove_nthNode_EndOfLL_19_LeetCode {
         head.next.next.next.next = new Node(5);
         head.next.next.next.next.next = new Node (6);
 
+        // Suppose we have to delete the node 3rd
+        // we need to remember that the question is asking to delete the node from the tail of the singly linked list
+        //setting Target Node as the question says us to do
+        int Target = 3;
+
+       Remove(head,Target);
+
+        System.out.println(head.next.next.next.next.val);
+
+
+    }
+    static void Remove (Node operation, int target){
+        // Approach called as Slow and Fast
+        Node slow = operation;
+
+        Node fast = operation;
+
+
+        while(target>=0){
+            fast=fast.next;
+            target--;
+
+        }
+        while((fast!=null) && (fast.next!=null)){
+            slow=slow.next;
+            fast=fast.next;
+
+
+
+        }
+        slow.next=slow.next.next;
+
     }
 }
